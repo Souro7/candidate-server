de0c5e3b26af170b63d8719f51bf2ef7b164cafc
Souro7 <sarkarsourobrata@gmail.com>
2021-04-25 15:16:38 +0530

    Add socket

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
+const ws = require('ws');
 
 let apiRoutes = require('./router');
 
 let app = express();
 
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+    socket.on('message', message => console.log(message));
+});
+
 var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from server'));
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log("Running server on port: "+ port);
 });
-//
+
+server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+        wsServer.emit('connection', socket, request);
+    });
+});
+
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
